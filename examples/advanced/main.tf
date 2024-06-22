18e281e41e7f2e029389608d38ee4105a4c7d96f
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-06-21 22:56:51 -0400

    chore(deps): update terraform terraform-ibm-modules/observability-instances/ibm to v2.13.2 (#120)

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "cloud_monitoring" {
   source            = "terraform-ibm-modules/observability-instances/ibm//modules/cloud_monitoring"
-  version           = "2.13.1"
+  version           = "2.13.2"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   instance_name     = "${var.prefix}-cm"
