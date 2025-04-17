f628e3f7030adbd11fec7d618d4d2e579ff3effe
ocofaigh <ocofaigh@ie.ibm.com>
2025-04-17 18:17:45 +0100

    clean

--- examples/enterprise/main.tf
+++ examples/enterprise/main.tf
@@ -119,13 +119,13 @@ module "trusted_profile_app_config_general" {
 locals {
   custom_role = "Template Assignment Reader"
 }
-# resource "ibm_iam_custom_role" "template_assignment_reader" {
-#   name         = "TemplateAssignmentReader"
-#   service      = "iam-identity"
-#   display_name = local.custom_role
-#   description  = "Custom role to allow reading template assignments"
-#   actions      = ["iam-identity.profile-assignment.read"]
-# }
+resource "ibm_iam_custom_role" "template_assignment_reader" {
+  name         = "TemplateAssignmentReader"
+  service      = "iam-identity"
+  display_name = local.custom_role
+  description  = "Custom role to allow reading template assignments"
+  actions      = ["iam-identity.profile-assignment.read"]
+}
 
 # Trusted Profile for App Config enterprise-level permissions
 module "trusted_profile_app_config_enterprise" {
