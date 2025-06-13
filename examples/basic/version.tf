bdf48a71469b90e69f38d66798db427600215c5e
akocbek <106765658+akocbek@users.noreply.github.com>
2025-06-13 15:11:15 +0100

    fix(deps): update ibm provider to 1.79.2 (#259)

--- examples/basic/version.tf
+++ examples/basic/version.tf
@@ -4,7 +4,7 @@ terraform {
     # Pin to the lowest provider version of the range defined in the main module's version.tf to ensure lowest version still works
     ibm = {
       source  = "ibm-cloud/ibm"
-      version = "1.79.0"
+      version = "1.79.2"
     }
     restapi = {
       source  = "Mastercard/restapi"
