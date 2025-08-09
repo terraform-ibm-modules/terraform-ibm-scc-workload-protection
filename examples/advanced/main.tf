47171df51817bdb6c28006765371dcaaa8bc2066
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-08-09 13:19:13 +0000

    chore(deps): update dependencies

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "cloud_monitoring" {
   source            = "terraform-ibm-modules/cloud-monitoring/ibm"
-  version           = "1.6.3"
+  version           = "1.6.4"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   instance_name     = "${var.prefix}-cm"
@@ -60,7 +60,7 @@ module "cbr_zone" {
 # Create new App Config instance
 module "app_config" {
   source                                 = "terraform-ibm-modules/app-configuration/ibm"
-  version                                = "1.8.14"
+  version                                = "1.8.15"
   region                                 = var.region
   resource_group_id                      = module.resource_group.resource_group_id
   app_config_plan                        = "basic"
