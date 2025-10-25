84d92e87f010622592d7101ac3ff0e45e7a0089e
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-10-25 05:55:27 +0000

    fix(deps): update dependencies

--- examples/enterprise/main.tf
+++ examples/enterprise/main.tf
@@ -15,7 +15,7 @@ module "resource_group" {
 
 module "app_config" {
   source                                                     = "terraform-ibm-modules/app-configuration/ibm"
-  version                                                    = "1.13.1"
+  version                                                    = "1.13.2"
   region                                                     = var.region
   resource_group_id                                          = module.resource_group.resource_group_id
   app_config_plan                                            = "basic"
