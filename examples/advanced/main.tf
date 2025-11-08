a9b490aa5f98ea8fe8d40862e94f27d497ecdf61
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-11-08 15:12:07 +0000

    chore(deps): update terraform terraform-ibm-modules/cloud-monitoring/ibm to v1.10.8

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "cloud_monitoring" {
   source            = "terraform-ibm-modules/cloud-monitoring/ibm"
-  version           = "1.10.7"
+  version           = "1.10.8"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   instance_name     = "${var.prefix}-cm"
