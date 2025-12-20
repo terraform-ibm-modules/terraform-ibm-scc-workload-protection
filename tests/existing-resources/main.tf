d7e9c9bdb3df9efda4c41e5055f77bca130694ad
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-12-20 15:14:45 +0000

    fix(deps): update dependencies

--- tests/existing-resources/main.tf
+++ tests/existing-resources/main.tf
@@ -4,7 +4,7 @@
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.4.6"
+  version = "1.4.7"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
@@ -17,7 +17,7 @@ module "resource_group" {
 # Create new App Config instance
 module "app_config" {
   source                                 = "terraform-ibm-modules/app-configuration/ibm"
-  version                                = "1.14.3"
+  version                                = "1.14.4"
   region                                 = var.region
   resource_group_id                      = module.resource_group.resource_group_id
   app_config_name                        = "${var.prefix}-app-config"
