512dfd90d0f65f77fec16bc846cf01e031d54f1f
ocofaigh <ocofaigh@ie.ibm.com>
2026-01-08 15:59:24 +0000

    clean

--- examples/enterprise/main.tf
+++ examples/enterprise/main.tf
@@ -74,12 +74,12 @@ resource "sysdig_secure_posture_zone" "example" {
   description = "Zone description"
   policy_ids  = [for p in local.fedramp_policies : p.id]
 
-  # you can use a scope to only target a set of accounts, e.g.
+  # you can use a scope to only target a set of sub-accounts by uncommenting the below code and updating the account IDs
 
   # scopes {
   #   scope {
   #     target_type = "ibm"
-  #     rules       = "account in (\"abac0df06b644a9cabc6e44f55b3880e\", \"9f9af00a96104f49b6509aa715f9d6a5\")"
+  #     rules       = "account in (\"nbac0df06b644a9cabc6e44f55b3880h\", \"5f9af00a96104f49b6509aa715f9d6a4\")"
   #   }
   # }
 }
