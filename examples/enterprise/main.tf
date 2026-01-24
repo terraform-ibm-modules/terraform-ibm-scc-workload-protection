1eaff95984fc8be52728e372eac70234a3a9c474
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-01-24 23:24:56 +0000

    chore(deps): update terraform terraform-ibm-modules/app-configuration/ibm to v1.14.15

--- examples/enterprise/main.tf
+++ examples/enterprise/main.tf
@@ -15,7 +15,7 @@ module "resource_group" {
 
 module "app_config" {
   source                                                     = "terraform-ibm-modules/app-configuration/ibm"
-  version                                                    = "1.14.14"
+  version                                                    = "1.14.15"
   region                                                     = var.region
   resource_group_id                                          = module.resource_group.resource_group_id
   app_config_plan                                            = "basic"
