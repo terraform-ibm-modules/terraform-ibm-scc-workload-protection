a1c2aea6380df03879300c803e537c8b7d38b356
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-01-25 04:19:02 +0000

    chore(deps): update terraform terraform-ibm-modules/app-configuration/ibm to v1.14.15 (#387)

--- tests/existing-resources/main.tf
+++ tests/existing-resources/main.tf
@@ -17,7 +17,7 @@ module "resource_group" {
 # Create new App Config instance
 module "app_config" {
   source                                 = "terraform-ibm-modules/app-configuration/ibm"
-  version                                = "1.14.14"
+  version                                = "1.14.15"
   region                                 = var.region
   resource_group_id                      = module.resource_group.resource_group_id
   app_config_name                        = "${var.prefix}-app-config"
