c0c2a2e7c5944382bfcf599161a598c887b50f4b
Arya Girish K <arya.girish.k@ibm.com>
2026-01-28 10:04:16 +0530

    testing

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "scc_wp" {
   source            = "terraform-ibm-modules/scc-workload-protection/ibm"
-  version           = "latest"
+  version           = "1.16.25"
   name              = var.prefix
   region            = var.region
   resource_group_id = module.resource_group.resource_group_id
