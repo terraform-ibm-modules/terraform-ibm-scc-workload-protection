ba20ed0adafaab09417a75ec1c8fe91d2b64ee43
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-02-22 02:22:45 +0000

    chore(deps): update terraform terraform-ibm-modules/app-configuration/ibm to v1.15.10

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -64,7 +64,7 @@ module "cbr_zone" {
 # Create new App Config instance
 module "app_config" {
   source                                 = "terraform-ibm-modules/app-configuration/ibm"
-  version                                = "1.15.8"
+  version                                = "1.15.10"
   region                                 = var.region
   resource_group_id                      = module.resource_group.resource_group_id
   app_config_plan                        = "basic"
